8223f57459ad4ab23669e69c12dadd864c784896
joinsachinarya <sachinkarya333@gmail.com>
2025-02-10 09:29:08 +0530

    revese linked list

--- linked-list/middle-element-of-ll.js
+++ linked-list/middle-element-of-ll.js
@@ -1,17 +1,53 @@
 class Node {
-    constructor(data){
+    constructor(data) {
         this.data = data;
         this.next = null
     }
 }
 
 
-class LL{
-    constructor(node = null){
+class LL {
+    constructor(node = null) {
         this.head = node;
     }
 
-    
+    findMiddle() {
+
+        // first approach : find length and then find length/2th element
+
+        // let size = this.size()
+        // let middle = Math.floor(size / 2);
+        // let current = this.head;
+        // let i = 0;
+        // while (i < middle) {
+        //     current = current.next;
+        //     i++;
+        // }
+        // return current
+
+
+        // second approach : tortoise and heir ( slow , fast pointer)
+        let slow = this.head;
+        let fast = this.head;
+        while (fast !== null && fast.next !== null) {
+            slow = slow.next;
+            fast  = fast.next.next;
+        }
+        return slow;
+    }
+    size() {
+        let current = this.head;
+        let size = 0;
+        if (this.head === null) {
+            return 0;
+        } else {
+            while (current !== null) {
+                size++;
+                current = current.next
+            }
+        }
+        return size
+    }
 
 }
 
@@ -22,4 +58,4 @@ const node3 = new Node(200);
 node.next = node2
 node2.next = node3
 const linkedList = new LL(node);
-console.log(JSON.stringify(linkedList))
+console.log(JSON.stringify(linkedList), linkedList.size(), linkedList.findMiddle())
